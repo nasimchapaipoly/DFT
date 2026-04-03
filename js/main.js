3217d52896c9e5bc28208186cbe3b8484d4985ab
nasimchapaipoly <nasim.chapaipoly@gmail.com>
2026-04-04 02:59:15 +0600

    Refactor header/footer loading and add toggle functions
    Refactored fetch calls for header and footer to use block syntax. Added global functions for sidebar and theme toggling.

--- js/main.js
+++ js/main.js
@@ -1,14 +1,24 @@
 // Load Header
 fetch('components/header.html')
   .then(res => res.text())
-  .then(data => document.getElementById('header').innerHTML = data);
+  .then(data => {
+    document.getElementById('header').innerHTML = data;
+  });
 
 // Load Footer
 fetch('components/footer.html')
   .then(res => res.text())
-  .then(data => document.getElementById('footer').innerHTML = data);
+  .then(data => {
+    document.getElementById('footer').innerHTML = data;
+  });
 
-// Sidebar toggle
-function toggleMenu() {
-  document.querySelector('.sidebar').classList.toggle('active');
-}
+// Sidebar Toggle (GLOBAL)
+window.toggleMenu = function () {
+  const sidebar = document.querySelector('.sidebar');
+  sidebar.classList.toggle('active');
+};
+
+// Theme Toggle (NEW)
+window.toggleTheme = function () {
+  document.body.classList.toggle('light');
+};
